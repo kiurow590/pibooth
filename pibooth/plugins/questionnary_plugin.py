34d4332fe62eb323a1feed491c4061e22304b46f
Kiurow590 <kiurow.geek@gmail.com>
2024-01-18 17:44:22 +0100

    Ajout du plugin pour les questionnaires cependant probleme pour trouver comment mettre tout le systeme en place

---
+++ pibooth/plugins/questionnary_plugin.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Pibooth plugin to display a question for users before taking pictures.
+ONLY IF HE GAVE A GOOD ANSWER HE CAN TAKE PICTURES
+"""
+
+import time
+import pygame
+import pibooth
+from pibooth import pictures, fonts
+
+__version__ = "0.0.2"
+
+
