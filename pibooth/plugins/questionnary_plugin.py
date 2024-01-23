323c95c4b78b917cd28bb9581b82ab12e366b473
Kiurow590 <kiurow.geek@gmail.com>
2024-01-23 18:26:49 +0100

    Mise en place de l'object questionnaire. A voir pour l'implé de tout le process...

--- pibooth/plugins/questionnary_plugin.py
+++ pibooth/plugins/questionnary_plugin.py
@@ -1,14 +1,27 @@
-# -*- coding: utf-8 -*-
-
-"""Pibooth plugin to display a question for users before taking pictures.
-ONLY IF HE GAVE A GOOD ANSWER HE CAN TAKE PICTURES
-"""
-
-import time
-import pygame
+from pibooth.utils import LOGGER
 import pibooth
-from pibooth import pictures, fonts
-
-__version__ = "0.0.2"
 
 
+class QuestionaryPlugin(object) :
+    name = 'pibooth-core:question'
+    
+    def __init__(self, plugin_manager):
+        self._pm = plugin_manager
+        LOGGER.info("Questionary plugin loaded")
+        
+    @pibooth.hookimpl
+    def state_previewQuestion_enter(cfg, app):
+        app.leds.blink(on_time=0.5, off_time=0.5)
+        
+    @pibooth.hookimpl
+    def state_previewQuestion_exit(self, app):
+        app.leds.off()
+        
+    @pibooth.hookimpl
+    def state_previewQuestion_do(cfg, app, win, events):
+        LOGGER.info("Question state")
+        
+        
+    @pibooth.hookimpl
+    def state_previewQuestion_validate(self, app, events):
+        LOGGER.info("Question state")
